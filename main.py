5d73f185b40fd90851ab202aa794e2e6f975fa50
rakhimzhon.dovidov <rkdovidov@rt-dc.ru>
2024-03-04 09:14:17 +0300

    feat: add sqlachemy orm

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import asyncio
+import os
 
+import telebot
 from sqlalchemy.ext.asyncio import create_async_engine
+import logging
 
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
 from bot.query_dispatcher import CallbackQueryDispatcher
 from telebot.async_telebot import AsyncTeleBot
 
@@ -27,6 +32,8 @@ setup_handlers(bot)
 async def async_main() -> None:
 
     async with engine.begin() as conn:
+        if os.name == "nt":
+            await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     await bot.infinity_polling()
 
@@ -34,4 +41,7 @@ async def async_main() -> None:
 
 
 if __name__ == '__main__':
+    if os.name == "nt":
+        print("Данная операционная система не поддерживается, приложение будет работать в демо режиме. "
+              "Для полноценной работы функционала необходимо запускать приложение в среде Linux")
     asyncio.run(async_main())
--- services/database/models.py
+++ services/database/models.py
@@ -1,14 +1,12 @@
-import asyncio
 import datetime
 from enum import StrEnum
-from typing import Optional, Literal
+from typing import Optional
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
-from services.print_service import OrientationEnum
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -18,6 +16,11 @@ class Base(AsyncAttrs, DeclarativeBase):
 engine = create_async_engine(f"sqlite+aiosqlite:///database.db", echo=True)
 
 
+class TaskStatusEnum(StrEnum):
+    WAITING = "WAITING"
+    PENDING = "PENDING"
+
+
 class Tasks(Base):
     __tablename__ = "tasks"
 
@@ -25,13 +28,14 @@ class Tasks(Base):
     uuid: Mapped[str]
     user_id: Mapped[int]
     chat_id: Mapped[int]
+    reply_id: Mapped[int]
     create_date: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
     printer_name: Mapped[str]
     copies: Mapped[int] = mapped_column(default=1)
     pages: Mapped[Optional[str]] = mapped_column(nullable=True)
     orientation: Mapped[int]
     file_path: Mapped[str]
-
+    status: Mapped[TaskStatusEnum] = mapped_column(default=TaskStatusEnum.WAITING)
 
 # class SavedPrintSettings(Base):
 #     __tablename__ = "b"
