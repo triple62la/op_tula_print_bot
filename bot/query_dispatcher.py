19312d325aaba64b8a19580b21ebfb0459fab880
rakhimzhon.dovidov <rkdovidov@rt-dc.ru>
2024-03-07 09:32:33 +0300

    feat: more buttons & functionality

--- bot/query_dispatcher.py
+++ bot/query_dispatcher.py
@@ -1,10 +1,13 @@
+import asyncio
+
 import telebot.async_telebot
 import telebot.types as bot_types
 
-from bot.inline_keyboards import create_finished_keyboard
+from bot.inline_keyboards import create_finished_keyboard, create_settings_keyboard, create_printers_keyboard
 from bot.message_handlers import compile_primary_msg
 from services.database.db_service import db_get_task_by_id, db_remove_task_by_id
 from services.database.models import TaskStatusEnum
+from services.file_service import delete_file
 from services.print_service import PrintSettings, OrientationEnum, exec_task
 
 
@@ -17,22 +20,28 @@ class CallbackQueryDispatcher:
         self.bot.register_callback_query_handler(callback=self.dispatch_callback_query, func=lambda callback: callback)
 
     async def dispatch_callback_query(self, callback: bot_types.CallbackQuery) -> None:
+
         action, uid = callback.data.split("#")
+        task = await db_get_task_by_id(uid)
         match action:
             case "execute":
-                await self.actions.on_execute(uid)
+                await self.actions.on_execute(uid, task)
             case "setup":
-                pass
+                await self.actions.on_setup(uid, task)
             case "delete":
-                await self.actions.on_delete(uid)
+                await self.actions.on_delete(uid, task)
+            case "setup_printer":
+                await self.actions.on_setup_printer(uid, task)
+            case "go_back":
+                pass
+
 
 class Actions:
 
     def __init__(self, bot: telebot.async_telebot.AsyncTeleBot):
         self.bot = bot
 
-    async def on_execute(self, uid):
-        task = await db_get_task_by_id(uid)
+    async def on_execute(self, uid, task):
         pages = task.pages.split(", ")
         settings = PrintSettings(task.printer_name, task.copies, pages, task.orientation)
         await exec_task(settings, task.file_path)
@@ -43,7 +52,22 @@ class Actions:
                                                              task_status=TaskStatusEnum.PENDING),
                                          task.chat_id, task.reply_id
                                          )
-
+        await asyncio.sleep(300)
         await db_remove_task_by_id(uid)
-    async def on_delete(self, uid):
-        task = await db_get_task_by_id(uid)
+        delete_file(task.file_path)
+        await self.bot.delete_message(task.chat_id, task.message_id)
+        await self.bot.delete_message(task.chat_id, task.reply_id)
+
+    async def on_delete(self, uid, task):
+        delete_file(task.file_path)
+        await db_remove_task_by_id(uid)
+        await self.bot.edit_message_text("Задание было отменено. Файл удален с сервера", task.chat_id, task.reply_id)
+
+    async def on_setup(self, uid, task):
+        await self.bot.edit_message_text("Параметры печати", task.chat_id, task.reply_id,
+                                         reply_markup=create_settings_keyboard(uid))
+
+    async def on_setup_printer(self, uid, task):
+        await self.bot.edit_message_text("Выберите принтер для печати текущего задания:", task.chat_id, task.reply_id,
+                                         reply_markup=create_printers_keyboard(uid))
+        self.bot.register_
